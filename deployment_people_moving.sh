c3549c9cc3f1516ac7b91077215ba379eb0b2148
Ekky Rino <ekky.rhino@gmail.com>
2024-08-01 09:59:49 +0700

    bug fixed on deployment scripts

--- deployment_people_moving.sh
+++ deployment_people_moving.sh
@@ -2,5 +2,5 @@
 NV="v0.3"
 docker build -f Dockerfile_people_moving -t people_moving:$NV .
 docker rm people_moving
-docker run --log-opt max-size=10m --log-opt max-file=3 -e CCTV_AREA="sol" --name people_moving -v /mnt/data/machine_learning/output/people_moving:/app/output/people_moving --gpus all -d --restart unless-stopped people_moving:v0.2
-# docker run --log-opt max-size=10m --log-opt max-file=3 -e CCTV_AREA="sol" --name people_moving -v /mnt/data/machine_learning/output/people_moving:/app/output/people_moving --gpus all --rm -it people_moving:v0.2 /bin/bash
+docker run --log-opt max-size=10m --log-opt max-file=3 -e CCTV_AREA="sol" --name people_moving -v /mnt/data/machine_learning/output/people_moving:/app/output/people_moving --gpus all -d --restart unless-stopped people_moving:$NV
+# docker run --log-opt max-size=10m --log-opt max-file=3 -e CCTV_AREA="sol" --name people_moving -v /mnt/data/machine_learning/output/people_moving:/app/output/people_moving --gpus all --rm -it people_moving:$NV /bin/bash
--- deployment_visitor_counter.sh
+++ deployment_visitor_counter.sh
@@ -2,5 +2,5 @@
 NV="v0.6"
 docker build -f Dockerfile_visitor_counter -t visitor_counter:$NV .
 docker rm visitor_counter
-docker run --log-opt max-size=10m --log-opt max-file=3 -e CCTV_AREA="sol" --name visitor_counter -v /mnt/data/machine_learning/output/visitor_counter:/app/output/visitor_counter --gpus all -d --restart unless-stopped visitor_counter:v0.5
-# docker run --log-opt max-size=10m --log-opt max-file=3 -e CCTV_AREA="sol" --name visitor_counter -v /mnt/data/machine_learning/output/visitor_counter:/app/output/visitor_counter --gpus all --rm -it visitor_counter:v0.5 /bin/bash
+docker run --log-opt max-size=10m --log-opt max-file=3 -e CCTV_AREA="sol" --name visitor_counter -v /mnt/data/machine_learning/output/visitor_counter:/app/output/visitor_counter --gpus all -d --restart unless-stopped visitor_counter:$NV
+# docker run --log-opt max-size=10m --log-opt max-file=3 -e CCTV_AREA="sol" --name visitor_counter -v /mnt/data/machine_learning/output/visitor_counter:/app/output/visitor_counter --gpus all --rm -it visitor_counter:$NV /bin/bash
