355c9dd674e8fbed766bd2375885e3750afd524e
Ekky Rino <ekky.rhino@gmail.com>
2024-08-05 12:32:00 +0700

    Create rm_old_video_people_moving.sh

---
+++ rm_old_video_people_moving.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Format: yyyymmdd ex: 20240803
+RM_DATE="20240802"
+
+rm -rf /mnt/data/machine_learning/output/people_moving/toilet/$RM_DATE
+rm -rf /mnt/data/machine_learning/output/people_moving/cig/$RM_DATE
+rm -rf /mnt/data/machine_learning/output/people_moving/stb/$RM_DATE
+rm -rf /mnt/data/machine_learning/output/people_moving/spklu/$RM_DATE
+rm -rf /mnt/data/machine_learning/output/people_moving/masjid/$RM_DATE
+rm -rf /mnt/data/machine_learning/output/people_moving/tsu/$RM_DATE
+rm -rf /mnt/data/machine_learning/output/people_moving/sol/$RM_DATE
+rm -rf /mnt/data/machine_learning/output/people_moving/mcd/$RM_DATE
